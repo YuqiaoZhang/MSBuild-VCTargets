f338ef460ce40f22fd317df21ef02e4c8f81773f
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-02-13 16:32:46 +0800

    Microsoft.Build.Utilities.Extension

---
+++ DotStandard/Microsoft.Build.CPPTasks.Common/src/CPPTasks/ArgumentRelation.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.Build.CPPTasks
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    public class ArgumentRelation : PropertyRelation
+    {
+        public ArgumentRelation(string argument, string value, bool required, string separator) : base(argument, value, required)
+        {
+            this.Separator = separator;
+        }
+
+        public string Separator { get; set; }
+    }
+}
+
---
+++ DotStandard/Microsoft.Build.Utilities.Extension/src/Utilities/TrackedDependencies/TrackedDependencies.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Shared;
+using Microsoft.Build.Shared.FileSystem;
+
+namespace Microsoft.Build.Utilities.Extension
+{
+    /// <summary>
+    /// This class contains utility functions to assist with tracking dependencies
+    /// </summary>
+    public static class TrackedDependencies
+    {
+        #region Methods
+        /// <summary>
+        /// This method checks that all the files exist
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns>bool</returns>
+        internal static bool ItemsExist(ITaskItem[] files)
+        {
+            bool allExist = true;
+
+            if (files != null && files.Length > 0)
+            {
+                foreach (ITaskItem item in files)
+                {
+                    if (!FileUtilities.FileExistsNoThrow(item.ItemSpec))
+                    {
+                        allExist = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                allExist = false;
+            }
+            return allExist;
+        }
+        #endregion
+    }
+}
