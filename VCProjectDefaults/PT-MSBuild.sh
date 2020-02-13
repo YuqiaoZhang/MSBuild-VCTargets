8a76efcccf6757582c30daacc80cde2c81eadb6b
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-02-14 04:21:00 +0800

    msbuild  verbosity level

--- VCProjectDefaults/PT-MSBuild.sh
+++ VCProjectDefaults/PT-MSBuild.sh
@@ -4,8 +4,8 @@ if [ $# -eq 2 ]; then
     MY_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" 
     # mono $MY_DIR/mono/msbuild/MSBuild.dll  
     # use Git Bash
-    echo "/c/Program Files/dotnet/dotnet" build $MY_DIR/PTSystem_PosixAndroid.vcxproj -property:VCTargetsPath=$MY_DIR/MSBuild/VCTargets/ -property:Configuration=$1 -property:Platform=$2
-    "/c/Program Files/dotnet/dotnet" build $MY_DIR/PTSystem_PosixAndroid.vcxproj -property:VCTargetsPath=$MY_DIR/MSBuild/VCTargets/ -property:Configuration=$1 -property:Platform=$2
+    echo "/c/Program Files/dotnet/dotnet" build -v:d $MY_DIR/PTSystem_PosixAndroid.vcxproj -property:VCTargetsPath=$MY_DIR/MSBuild/VCTargets/ -property:Configuration=$1 -property:Platform=$2
+    "/c/Program Files/dotnet/dotnet" build -v:d $MY_DIR/PTSystem_PosixAndroid.vcxproj -property:VCTargetsPath=$MY_DIR/MSBuild/VCTargets/ -property:Configuration=$1 -property:Platform=$2
 else
     echo Usage: PT-MSBuild.sh Configuration\(Debug\|Release\) Platform\(x86\|x64\)
 fi
