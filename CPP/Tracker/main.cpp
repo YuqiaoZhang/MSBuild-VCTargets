5889dfc5bf304e3938d3727969d407bfb30103f3
YuqiaoZhang <yuqiaozhang@aliyun.com>
2020-02-17 04:48:51 +0800

    File Tracker

--- CPP/Tracker/main.cpp
+++ CPP/Tracker/main.cpp
@@ -1,29 +1,143 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include <unistd.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <sys/ptrace.h>
 
-/* 
- * File:   main.cpp
- * Author: Administrator
- *
- * Created on February 14, 2020, 9:26 PM
- */
+#include <assert.h>
 
+#include <bits/wordsize.h>
+#include <sys/syscall.h>
+
+#include <sys/reg.h>
+
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
 #include <stdio.h>
 
-/*
- * 
- */
 int main(int argc, char **argv)
 {
-    for (int i = 0; i < argc; ++i)
+    pid_t pid;
+    if ((pid = ::fork()) == 0)
     {
-        printf("%s ",argv[i]);
+        // http://man7.org/linux/man-pages/man2/ptrace.2.html
+        // If the PTRACE_O_TRACEEXEC option is not in effect, all successful
+        // calls to execve(2) by the traced process will cause it to be sent a
+        // SIGTRAP signal, giving the parent a chance to gain control before the
+        // new program begins execution.
+        ::ptrace(PTRACE_TRACEME, 0L, 0L, 0L);
+
+        return ::execlp("cat", "/usr/include/stdio.h");
     }
+    else
+    {
+        int status;
+        ::waitpid(pid, &status, 0);
+        assert(WIFSTOPPED(status));
 
-    printf("\n");
+        //ptrace only works when tracee is stopped?
+        int i = ::ptrace(PTRACE_SETOPTIONS, pid, 0L, PTRACE_O_TRACESYSGOOD);
 
-    return 0;
+        for (int _7 = 0; _7 < 15; ++_7)
+        {
+            // http://man7.org/linux/man-pages/man2/ptrace.2.html
+            // Restart the stopped tracee as for PTRACE_CONT.
+            i = ::ptrace(PTRACE_SYSCALL, pid, 0L, 0L);
+            auto str2 = strerror(errno);
+
+            //Syscall Entry
+            //int status;
+            auto p2 = ::waitpid(pid, &status, 0);
+            bool isstopped = (WIFSTOPPED(status));
+            auto stopsig = (WSTOPSIG(status));
+
+            if ((WIFSTOPPED(status)) && ((WSTOPSIG(status)) == (SIGTRAP | 0X80)))
+            {
+#ifdef __x86_64__
+                // http://man7.org/linux/man-pages/man2/syscall.2.html
+                // System call #
+                // rax
+                auto systemcallnumber_entry = ::ptrace(PTRACE_PEEKUSER, pid, (__WORDSIZE / 8) * ORIG_RAX, 0L);
+#elif defined(__i386__)
+                //Compiler May Define Both __x86_64__ and __i386__ Because Of Bugs.
+
+#elif defined(__arm__)
+#elif defined(__aarch64__)
+#else
+#error Unknown Architecture 未知的架构
+#endif
+                switch (systemcallnumber_entry)
+                {
+                case SYS_open:
+                {
+#ifdef __x86_64__
+                    // http://man7.org/linux/man-pages/man2/syscall.2.html
+                    // arg1
+                    // rdi
+                    auto arg1 = ::ptrace(PTRACE_PEEKUSER, pid, (__WORDSIZE / 8) * RDI, 0L);
+#elif defined(__i386__)
+
+#elif defined(__arm__)
+#elif defined(__aarch64__)
+#else
+#error Unknown Architecture 未知的架构
+#endif
+                    char pathname[4096];
+                    for (int _w = 0; _w < (4096 / (__WORDSIZE / CHAR_BIT)); ++_w)
+                    {
+                        auto val4 = ::ptrace(PTRACE_PEEKTEXT, pid, arg1 + (__WORDSIZE / CHAR_BIT) * _w, 0L);
+
+                        bool meetzeroterm = false;
+                        for (int _c = 0; _c < (__WORDSIZE / CHAR_BIT); ++_c)
+                        {
+                            char val = reinterpret_cast<char *>(&val4)[_c];
+                            pathname[(__WORDSIZE / CHAR_BIT) * _w + _c] = val;
+                            if (val == '\0')
+                            {
+                                meetzeroterm = true;
+                                break;
+                            }
+                        }
+
+                        if (meetzeroterm)
+                        {
+                            break;
+                        }
+                    }
+                    if (pathname[1] == 'u')
+                    {
+                        printf("%s\n", pathname);
+                    }
+                }
+                break;
+                case SYS_openat:
+                    break;
+                default:
+                    break;
+                }
+
+                // Restart the stopped tracee as for PTRACE_CONT.
+                i = ::ptrace(PTRACE_SYSCALL, pid, 0L, 0L);
+
+                //Syscall Exit //Some Syscall(such as exec) not return?
+                p2 = ::waitpid(pid, &status, 0);
+
+#ifdef __x86_64__
+                auto systemcallnumber_exit = ::ptrace(PTRACE_PEEKUSER, pid, (__WORDSIZE / 8) * ORIG_RAX, 0L);
+#elif defined(__i386__)
+#elif defined(__arm__)
+#elif defined(__aarch64__)
+#else
+#error Unknown Architecture 未知的架构
+#endif
+                assert(systemcallnumber_entry == systemcallnumber_exit);
+            }
+            else
+            {
+                i = ::ptrace(PTRACE_SYSCALL, pid, 0L, 0L);
+            }
+        }
+
+        return 0;
+    }
 }
